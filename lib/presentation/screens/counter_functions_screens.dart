77c8c4ce0c88339be824912dc1a37471bccf4c02
LuisEmpanadas <luisvaldezjmz@gmail.com>
2026-01-26 19:22:57 -0600

    feat: Add decrement button, set limit to 0, change colors to purple

--- lib/presentation/screens/counter_functions_screens.dart
+++ lib/presentation/screens/counter_functions_screens.dart
@@ -13,31 +13,68 @@ class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Counter App'),
-        ),
-        body: Center(
-            child: Column(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                counter = 0;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               '$counter',
-              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+                color: Colors.purple,
+              ),
             ),
             Text(
-              'Clicks',
-              style: TextStyle(fontSize: 25),
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-              child: Text('Increment'),
+              counter == 1 ? 'Click' : 'Clicks',
+              style: const TextStyle(
+                fontSize: 25,
+                color: Colors.purple,
+              ),
             ),
           ],
-        )));
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                if (counter > 0) {
+                  counter--;
+                }
+              });
+            },
+            backgroundColor: Colors.purple,
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+            backgroundColor: Colors.purple,
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
   }
 }
