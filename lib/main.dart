5e1e48078f4a6fc4c10a898f88cb498b04caf760
LuisEmpanadas <luisvaldezjmz@gmail.com>
2026-01-29 12:09:23 -0600

    feat: add theme toggle button (light/dark) and wire to MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,45 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void _toggleTheme() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 178, 19, 226)),
+          seedColor: const Color.fromARGB(255, 178, 19, 226),
+        ),
       ),
-      home: const CounterApp(),
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 178, 19, 226),
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: _themeMode,
+      home: CounterApp(
+        onToggleTheme: _toggleTheme,
+        isDarkMode: _themeMode == ThemeMode.dark,
+      ),
     );
   }
 }
--- lib/presentation/screens/counter_functions_screens.dart
+++ lib/presentation/screens/counter_functions_screens.dart
@@ -1,80 +1,89 @@
 import 'package:flutter/material.dart';
 
 class CounterApp extends StatefulWidget {
-  const CounterApp({super.key});
+  class CounterApp extends StatefulWidget {
+    const CounterApp({super.key, required this.onToggleTheme, required this.isDarkMode});
 
-  @override
-  State<CounterApp> createState() => _CounterAppState();
-}
+    final VoidCallback onToggleTheme;
+    final bool isDarkMode;
 
-class _CounterAppState extends State<CounterApp> {
-  int counter = 0;
+    @override
+    State<CounterApp> createState() => _CounterAppState();
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter App'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_rounded),
-            onPressed: () {
-              setState(() {
-                counter = 0;
-              });
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$counter',
-              style: TextStyle(
-                fontSize: 160,
-                fontWeight: FontWeight.w100,
-                color: counter == 12 ? Colors.purple : Colors.black,
-              ),
+  class _CounterAppState extends State<CounterApp> {
+    int counter = 0;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(widget.isDarkMode ? Icons.light_mode : Icons.dark_mode),
+              onPressed: widget.onToggleTheme,
+              tooltip: 'Toggle theme',
             ),
-            Text(
-              counter == 1 ? 'Click' : 'Clicks',
-              style: TextStyle(
-                fontSize: 25,
-                color: counter == 12 ? Colors.purple : Colors.black,
-              ),
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  counter = 0;
+                });
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                if (counter > 0) {
-                  counter--;
-                }
-              });
-            },
-            backgroundColor: Colors.purple,
-            child: const Icon(Icons.remove),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$counter',
+                style: TextStyle(
+                  fontSize: 160,
+                  fontWeight: FontWeight.w100,
+                  color: counter == 12 ? Colors.purple : Colors.black,
+                ),
+              ),
+              Text(
+                counter == 1 ? 'Click' : 'Clicks',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: counter == 12 ? Colors.purple : Colors.black,
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 16),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            },
-            backgroundColor: Colors.purple,
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
-    );
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  if (counter > 0) {
+                    counter--;
+                  }
+                });
+              },
+              backgroundColor: Colors.purple,
+              child: const Icon(Icons.remove),
+            ),
+            const SizedBox(height: 16),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  counter++;
+                });
+              },
+              backgroundColor: Colors.purple,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      );
+    }
   }
-}
